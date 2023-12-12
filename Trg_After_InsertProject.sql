059eabeadeaef854f367c476ced38711a19a08c9
Serhii Makovetskyi <xxxsergeyxxx77@gmail.com>
2023-12-12 12:50:51 +0200

    Cleaned unneeded files

--- Trg_After_InsertProject.sql
+++
@@ -1,8 +0,0 @@
---drop trigger trg_after_insert_project
-
-CREATE TRIGGER trg_after_insert_project
-ON Project AFTER INSERT
-as
-    -- Insert a new record into the Change_Log table
-    INSERT INTO Change_Log (Change_Log_ID, Project_ID, Project_Status, Change_Date, Change_Description)
-    VALUES ((SELECT COALESCE(MAX(Change_Log_Id), 0) + 1 FROM Change_Log), (select Project_ID FROM inserted), 1, GETDATE(), 'Project Created');
--- View_Media_Info.sql
+++
@@ -1,5 +0,0 @@
-CREATE VIEW Media_Info As
-SELECT Review_ID as 'Review ID', Project.Project_ID as 'Project ID', Project_Name as 'Project title', 
-Review_Link as 'Review link', Review_Date as 'Review date'
-FROM  Project, Media_Reviews
-where Project.Project_ID = Media_Reviews.Project_ID
